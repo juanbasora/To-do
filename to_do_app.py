97491aaceec107698999e1b8744916a2ef07d6c4
Juan B. <juanbasora89@gmail.com>
2025-01-01 20:54:34 -0500

    Update to_do_app.py

--- to_do_app.py
+++ to_do_app.py
@@ -48,19 +48,6 @@ class Add_new_task(customtkinter.CTkToplevel):
         self.due_date_label.grid(row=2, column=0)
         self.due_date_entry = DateEntry(master=self, width=12, normalbackground="grey28", weekendbackground="grey", headersforeground="white", background="grey", borderwidth=2)
         self.due_date_entry.grid(row=2, column=1, columnspan=2, pady=10)
-        
-        # Got the calendar widget to work but it is not the best looking widget. Need to fix the colors.
-
-        # self.due_date_entry = customtkinter.CTkEntry(master=self)
-        # self.due_date_entry.grid(row=2, column=1)
-
-        # convert the date string into a date object.
-        # https://www.digitalocean.com/community/tutorials/python-string-to-datetime-strptime
-
-
-        # replace this with a calendar widget
-        # use the widget from the ttkbootstrap library
-        # youtube video: https://www.youtube.com/watch?v=jACXHXaGLqQ
 
         self.notes_label = customtkinter.CTkLabel(master=self, text="Notes: ")
         self.notes_label.grid(row=3, column=0)
@@ -68,9 +55,6 @@ class Add_new_task(customtkinter.CTkToplevel):
         self.notes_entry = customtkinter.CTkTextbox(master=self, fg_color="gray22")
         self.notes_entry.grid(row=3, column=1)
 
-
-        # submit button
-        # need to add this button to the if statement below so it can be submitted or updated.
         self.submit_button = customtkinter.CTkButton(master=self, text="Submit", fg_color="transparent", text_color=("gray10", "gray90"), command=self.add_task)
         self.submit_button.grid(row=7, column=0, columnspan=3, sticky="s")
 
@@ -82,10 +66,6 @@ class Add_new_task(customtkinter.CTkToplevel):
         self.priority_dropdown.set(priority)
 
         self.submit_button.configure(text="Update", command=self.update_task)
-
-        # Need to find the data in the database and get the id number to update the task.
-
-        # self.submit_button.configure(command=self.update_task if update else self.add_task)
 
     def update_task(self):
         name = self.task_entry_box.get()
@@ -99,13 +79,6 @@ class Add_new_task(customtkinter.CTkToplevel):
         else:
             print("Error: Task not updated")
 
-# ----------------------------------------------------------------------------------------------------
-# Need to fix the edit function on the back end. It's mixing up the information.  
-# Descriotion is being saved as the due date and the due date is the description.  
-# 
-# Fixed the issue by changing the order of the variables in the update function in the backend.py file.    
-# ----------------------------------------------------------------------------------------------------
-
     def add_task(self):
         name = self.task_entry_box.get()
         priority = self.priority_dropdown.get()
@@ -113,33 +86,15 @@ class Add_new_task(customtkinter.CTkToplevel):
         notes = self.notes_entry.get(0.0, "end")
         completed = "no"
 
-        # Validate the input
+        # Validate the input, work in progress.
         if name == "" or due_date == "":
             print("Please enter all required information")
             # Need to change this to a pop up window
 
-        # how to check if the date is in the correct format
-#         from datetime import datetime
-
-        # print(name, priority, due_date, notes, completed)
         self.database.add_entry(due_date, name, notes, priority, completed)
-        # Now that I think this works, I need to add a way to close the window after the submit button is clicked.
         self.task_entry_box.delete(0, "end")
         self.due_date_entry.delete(0, "end")
         self.notes_entry.delete(0.0, "end")
-
-# ----------------------------------------------------------------------------------------------------
-# Need to add a way to close the window after the submit button is clicked.
-# add a radio button labeled "mutiple tasks" that will allow the user to add multiple tasks at once.
-# if not selected then the window will close after the submit button is clicked.
-# ----------------------------------------------------------------------------------------------------
-
-
-
-# This will create a new window and lets you add a new task.
-# Need to set the parent window or set this new window as a top layer window
-# so that you cannot click back on the old window without closing this one first.
-
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -179,18 +134,9 @@ class App(customtkinter.CTk):
         self.scrollable_label_button_frame.grid(row=0, column=0, sticky="nsew")
         self.scrollable_label_button_frame.grid_rowconfigure(4, weight=1)
 
-        # ----------------------------------------------------------------------------------------------------
-        # probably need to change the code above to a function that can be called to update the buttons
-        # A True or False value can be passed to the function to update the buttons.
-        # ----------------------------------------------------------------------------------------------------
-
         # create a frame for the buttons
         self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
         self.home_frame.grid_columnconfigure(0, weight=1)
-
-        # need to add a way to update the buttons when a task is completed.
-        # need to add a way to update the buttons when a task is deleted.
-
     
         # Building a second frame to display the task details
         self.second_frame = customtkinter.CTkFrame(master=self)
@@ -204,8 +150,7 @@ class App(customtkinter.CTk):
         self.complete_button.grid_forget()
 
 
-        # Add the stuff here for the second frame then hide them until a button is clicked.
-
+        # Add the widgets here for the second frame then hide them until a button is clicked.
         self.name_label = customtkinter.CTkLabel(master=self.second_frame)
         self.name_label.grid(row=0, column=0, padx=10)
         self.name_label.grid_forget()
@@ -251,8 +196,6 @@ class App(customtkinter.CTk):
 
         for button_names in self.names:
             self.scrollable_label_button_frame.add_button(button_names)
-        
-
 
     def label_button_frame_event(self, task_name):
         global entries
@@ -284,12 +227,6 @@ class App(customtkinter.CTk):
         for button in button_list:
             button.configure(fg_color=("gray75", "gray25") if button == button_list[number] else "transparent")
 
-            # Since I don't have the butons pre set in variables
-            # I am saving a list with all buttons names and when a button is slected.
-            # the program looks for that button name in the list and gets the index location
-            # then I just configure the button in the button list wit the same index number
-            # so button name one in the list will be the first button.
-
     def open_new_window(self):
         if customtkinter.CTkToplevel is None or not customtkinter.CTkToplevel.winfo_exists():
             customtkinter.CTkToplevel = Add_new_task()
@@ -308,8 +245,6 @@ class App(customtkinter.CTk):
         description = entries[number][4]
         due_date = entries[number][1]
         priority = entries[number][3]
-
-        # print(f"print name: {name}, description: {description}, due date: {due_date}, priority: {priority}")
 
         for task in self.database.view_all():
             if task[2] == name and task[4] == description and task[1] == due_date:
